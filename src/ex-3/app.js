6014cdc501ef8f29ad9f7dbc927b94d03d7030bb
eyadcode24 <eyadcode24@gmail.com>
2024-05-28 07:45:19 +0200

    2

--- src/ex-3/app.js
+++ src/ex-3/app.js
@@ -2,6 +2,20 @@
 //cuando rellena en el formulario con su nombre y valida 
 //haciendo clik en el boton vañidar
 //entonces se renderiza el texto "hola <nombre de usuario>"
-
+//
 //click de boton = evento
 //recuperar el nombre de usuario
+//rendenizar en yn contenedor html el texto "hola pepe"
+
+const inputUsername = document.getElementById('username')
+console.log(inputUsername.value);
+const btnValidate = document.getElementById('btn-validate')
+const mainBlock = document.querySelector('main')
+
+btnValidate.addEventListener('click') => {
+
+const username = input-username.value
+console.log(username);
+
+
+}
