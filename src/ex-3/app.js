831d2f424aac81e0daadc408785ee1a4fc26cee6
eyadcode24 <eyadcode24@gmail.com>
2024-05-28 08:21:27 +0200

    33

--- src/ex-3/app.js
+++ src/ex-3/app.js
@@ -2,20 +2,38 @@
 //cuando rellena en el formulario con su nombre y valida 
 //haciendo clik en el boton vañidar
 //entonces se renderiza el texto "hola <nombre de usuario>"
-//
+//dado el input esta vacio 
+//cuando el usuario hace clish en validate 
+//entonces se muestra el text "debe introducir su nombre"
+
+//creterio 1
 //click de boton = evento
 //recuperar el nombre de usuario
 //rendenizar en yn contenedor html el texto "hola pepe"
+//creterio 2
+//conprobar si el input esta vacio
+//
 
 const inputUsername = document.getElementById('username')
 console.log(inputUsername.value);
 const btnValidate = document.getElementById('btn-validate')
 const mainBlock = document.querySelector('main')
 
+let isEmpty = true
 btnValidate.addEventListener('click') => {
 
-const username = input-username.value
-console.log(username);
-
+    const username = input-username.value
+    console.log(username.length)
+    console.log(username);
+    mainBlock.innerHTML= /*html*/
+        `<span>Hola usernme</span>`
+}
+funcioncrender(){
+    mainBlock.innerHTML = /*html*/`
+        <span>Hola ${username}/span>`
+}
+function ckeckInput(){
+    const stringLength = inputUsername.value.length
 
 }
+1.18
